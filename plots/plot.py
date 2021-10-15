2c218f7a2b3648191b0ed4e646c4a0524f934786
devansh-dvj <devansh.dvj@gmail.com>
2021-10-15 11:19:02 +0530

    Added IPC data

--- plots/plot.py
+++ plots/plot.py
@@ -4,24 +4,40 @@ import matplotlib.pyplot as plt
 
 ipc = {
     'bfs.trace.xz': {
-        'la': 1,
-        'lb': 2,
-        'lc': 3
+        'baseline': 0.869183,
+        'direct-mapped': 0.865938,
+        'fully-associated': 0.86877,
+        'reduced-size': 0.858612,
+        'doubled-size': 0.876374,
+        'reduced-mshr': 0.869172,
+        'doubled-mshr': 0.86919,
     },
     'matrix_multi.trace.xz': {
-        'la': 1,
-        'lb': 2,
-        'lc': 3
+        'baseline': 0.567407,
+        'direct-mapped': 0.565071,
+        'fully-associated': 0.567407,
+        'reduced-size': 0.56736,
+        'doubled-size': 0.567404,
+        'reduced-mshr': 0.567394,
+        'doubled-mshr': 0.567409,
     },
     'matrix_multi_2.trace.xz': {
-        'la': 1,
-        'lb': 2,
-        'lc': 3
+        'baseline': 0.567465,
+        'direct-mapped': 0.565143,
+        'fully-associated': 0.567465,
+        'reduced-size': 0.567424,
+        'doubled-size': 0.567461,
+        'reduced-mshr': 0.567452,
+        'doubled-mshr': 0.567464,
     },
     'quicksort.trace.xz': {
-        'la': 1,
-        'lb': 2,
-        'lc': 3
+        'baseline': 0.426019,
+        'direct-mapped': 0.311887,
+        'fully-associated': 0.426019,
+        'reduced-size': 0.425672,
+        'doubled-size': 0.423997,
+        'reduced-mshr': 0.426019,
+        'doubled-mshr': 0.426019,
     },
 }
 
